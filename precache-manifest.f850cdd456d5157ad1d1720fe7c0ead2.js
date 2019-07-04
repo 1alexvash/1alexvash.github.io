f940d3fcb37d72f61283922800b9fd80761dfbd2
Alexander <1alexvash@gmail.com>
2019-07-04 09:35:59 +0300

    first commit

---
+++ precache-manifest.f850cdd456d5157ad1d1720fe7c0ead2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6494ab5b3760b5e2cc46f5566a245371",
+    "url": "/static/media/stars.6494ab5b.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "39f91fe487aa5a3ddb7b",
+    "url": "/static/js/main.39f91fe4.chunk.js"
+  },
+  {
+    "revision": "2c18bbf0ce1443bfd023",
+    "url": "/static/js/2.2c18bbf0.chunk.js"
+  },
+  {
+    "revision": "39f91fe487aa5a3ddb7b",
+    "url": "/static/css/main.dbd2333b.chunk.css"
+  },
+  {
+    "revision": "aa4d1f45a5a7e579fabe6c57f1bcc221",
+    "url": "/index.html"
+  }
+];
