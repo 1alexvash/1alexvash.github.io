7f2958187e746848ff141db5ec58013f25b27ff2
Alexander <1alexvash@gmail.com>
2021-03-13 08:17:25 +0200

    Updates

---
+++ precache-manifest.dd1bbec2428c2dc0e84290aa026712b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0881892e27af71d32b2",
+    "url": "/static/css/main.58d04573.chunk.css"
+  },
+  {
+    "revision": "d0881892e27af71d32b2",
+    "url": "/static/js/main.d0881892.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5fb43ae06073d9217e04",
+    "url": "/static/js/2.5fb43ae0.chunk.js"
+  },
+  {
+    "revision": "6494ab5b3760b5e2cc46f5566a245371",
+    "url": "/static/media/stars.6494ab5b.png"
+  },
+  {
+    "revision": "5ecd9f950209a3f8ae175b15e4399084",
+    "url": "/index.html"
+  }
+];
